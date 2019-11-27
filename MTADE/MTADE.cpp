f9232687c25ee6f9de6666635ff1e09dbe95a3cc
LouieHan <ibelivehl@163.com>
2019-11-27 20:58:21 +0800

    完成DE库改写，提供简单的圆方程求解目标函数案例

--- MTADE/MTADE.cpp
+++ MTADE/MTADE.cpp
@@ -2,10 +2,95 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <memory>
 
+#include "differential_evolution.hpp"
+#include "objective_function.h"
 
-int main()
+using namespace de;
+
+#define VARS_COUNT 20
+#define POPULATION_SIZE 200
+
+int main(int argc, char *argv[])
 {
+	try
+	{
+		/**
+		* 创建与初始化constraints对象
+		*
+		* 首先创建一个默认constraints对象(double type, min -1.0e6, max 1.0e6)，
+		* 然后将目标函数中的两个参数设置为实数类型：-10<=x<=10,-100<=y<=100.
+		*/
+		constraints_ptr constraints(std::make_shared< constraints >(VARS_COUNT, 1.0e-6, 1.0e6));
+		(*constraints)[0] = std::make_shared< real_constraint >(-10, 10);//width
+		(*constraints)[1] = std::make_shared< real_constraint >(-100, 100);//depth
+		(*constraints)[2] = std::make_shared< real_constraint >(-100, 100);//heigth
+
+		/**
+		* 实例化目标函数
+		*
+		* 目标函数可以是任何以de :: DVectorPtr作为参数并返回双精度的函数或函子。 它可以通过引用，指针或共享指针传递。
+		*/
+		sphere_function of;
+
+		/**
+		* 实例化两个空侦听器，一个用于差分进化，另一个用于处理器
+		*/
+		listener_ptr listener(std::make_shared< null_listener >());
+		processor_listener_ptr processor_listener(std::make_shared< null_processor_listener >());
+
+		/**
+		* 用并行处理器的数量（4），目标函数和侦听器实例化处理器的集合。
+		*/
+		processors< sphere_function >::processors_ptr _processors(std::make_shared< processors< sphere_function > >(4, std::ref(of), processor_listener));
+
+		/**
+		* 实例化一个简单的终止策略，它将在10000代之后停止优化过程。
+		*/
+		termination_strategy_ptr terminationStrategy(std::make_shared< max_gen_termination_strategy >(1000));
+
+		/**
+		* 实例化选择策略-我们将使用最好的父/子策略
+		*/
+		selection_strategy_ptr selectionStrategy(std::make_shared< best_parent_child_selection_strategy >());
+
+		/**
+		* 实例化变异策略-我们将使用变异策略1，权重和交叉因子分别设置为0.5和0.9
+		*/
+		mutation_strategy_arguments mutation_arguments(0.5, 0.9);
+		mutation_strategy_ptr mutationStrategy(std::make_shared< mutation_strategy_1 >(VARS_COUNT, mutation_arguments));
+
+		/**
+		* 使用先前定义的约束，处理器，侦听器和各种策略实例化差分进化。
+		*/
+		differential_evolution< sphere_function > de(VARS_COUNT, POPULATION_SIZE, _processors, constraints, true, terminationStrategy, selectionStrategy, mutationStrategy, listener);
+
+		/**
+		* 运行优化进程
+		*/
+		de.run();
+
+		/**
+		* 从优化过程中获得最佳个体
+		*/
+		individual_ptr best(de.best());
+
+		/**
+		* Print out the result
+		*/
+		std::cout << "minimum value for the " << /*of->name() << */" is " << best->cost() << " for x=" << (*best->vars())[0] << ", y=" << (*best->vars())[1]<<std::endl;
+	}
+	catch (const de::exception& e)
+	{
+		/**
+		* Print out any errors that happened during the initialization
+		* or optimization phases
+		*/
+		std::cout << "an error occurred: " << e.what();
+	}
+	
     return 0;
 }
 
---
+++ MTADE/objective_function.h
@@ -0,0 +1,118 @@
+#ifndef DE_OBJECTIVE_FUNCTION_HPP_INCLUDED
+#define DE_OBJECTIVE_FUNCTION_HPP_INCLUDED
+
+// MS compatible compilers support #pragma once
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1020)
+#pragma once
+#endif
+
+#include <memory>
+
+#include "de_types.hpp"
+#include "processors.hpp"
+
+/**
+* 具体目标函数的抽象基类。
+*
+* @author louiehan (11/12/2019)
+*/
+class objective_function
+{
+private:
+	const std::string m_name;
+public:
+	/**
+	* constructs an objective_function object
+	*
+	* @author louiehan (11/12/2019)
+	*
+	* @param name the objective function name
+	*/
+	objective_function(const std::string& name)
+		: m_name(name)
+	{
+	}
+
+	virtual ~objective_function() {}
+
+	/**
+	* 在派生类中实现-它包含要优化的目标函数。
+	*
+	* 目标函数接受一个double类型的参数向量并返回计算出的double值。
+	*
+	* args向量中的每个索引对应于约束向量中的相同索引。例如，如果目标函数需要两个变量，
+	* 则将约束向量的前两个元素设置为两个变量的约束，并在目标函数operator（）中使用
+	* args向量的前两个值作为两个变量。可以忽略此向量中的所有其他值。
+	*
+	* @author louiehan (11/12/2019)
+	*
+	* @param args 参数向量。向量通常比目标函数使用的变量数大得多，
+	*			  因此目标函数of只取向量中的前n个值，而忽略其余的值。
+	*
+	* @return double 函数代价，这是需要优化的值。
+	*/
+	virtual double operator()(de::DVectorPtr args) = 0;
+
+	/**
+	* An objective function has a name
+	*
+	* @author louiehan (11/12/2019)
+	*
+	* @return const std::string&
+	*/
+	const std::string& name() const { return m_name; }
+};
+
+/**
+* Smart pointer to an objective function
+*/
+typedef std::shared_ptr< objective_function > objective_function_ptr;
+
+/**
+* Objective function to optimize is the "sphere function":
+*
+* f(x,y) = x^2 + y^2
+*/
+class sphere_function:public objective_function
+{
+public:
+	sphere_function()
+		: objective_function( "sphere function" )
+	{
+	}
+
+	virtual double operator()(de::DVectorPtr args)
+	{
+		/**
+		* The two function arguments are the elements index 0 and 1 in
+		* the argument vector, as defined by the constraints vector
+		* below
+		*/
+		double x = (*args)[0];
+		double y = (*args)[1];
+
+		return x*x + y*y;
+	}
+};
+
+class evaluation_route :public objective_function
+{
+public:
+	evaluation_route()
+		: objective_function("Route evaluation function")
+	{
+	}
+
+	virtual double operator()(de::DVectorPtr args)
+	{
+		double x = (*args)[0];
+		double y = (*args)[1];
+		double z = (*args)[2];
+
+		return pow(x,3) + pow(y,3)+ pow(z,3);
+	}
+
+};
+
+#endif //DE_OBJECTIVE_FUNCTION_HPP_INCLUDED
